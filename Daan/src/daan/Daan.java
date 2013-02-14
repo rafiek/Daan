6912d4d333444a37c7cb6cdc37a363aec6d05da5
Rafiek Mohamedjoesoef <mohamedjoesoef@chello.nl>
2013-02-14 10:12:11 +0100

    setup of Netbeans project

---
+++ Daan/src/daan/Daan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daan;
+
+/**
+ *
+ * @author Rafiek Mohamedjoesoef <Rafiek.Mohamedjoesoef@hva.nl>
+ */
+public class Daan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
